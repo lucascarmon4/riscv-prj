e734bdd78db1e829b9834e2d47425a0db293dbdf
Felipe1501 <felipebsantos1501@gmail.com>
2025-11-15 20:56:45 -0300

    add addi, add and sub

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -41,32 +41,65 @@ void CPU::step(){
     uint32_t rd     = getBits(inst, 11, 7);
     uint32_t funct3 = getBits(inst, 14, 12);
     uint32_t rs1    = getBits(inst, 19, 15);
-    uint32_t imm12  = getBits(inst, 31, 20);
-    int32_t imm = signExtend(imm12, 12);
+    uint32_t rs2    = getBits(inst, 24, 20);
+    uint32_t funct7 = getBits(inst, 31, 25);
 
 
     std::cout << "[CPU] opcode= " << opcode 
               << ", rd=" << rd 
               << ", funct3=" << funct3 
               << ", rs1=" << rs1 
-              << ", imm=" << imm << std::endl;
+              << ", rs2=" << rs2
+              << ", funct7=" << funct7 << std::endl;
     
-    if (opcode == 0x13 && funct3 == 0x0) {
-        uint32_t old = x[rs1];
-        uint32_t result = old + imm;
+    if (opcode == 0x13){
+        uint32_t imm12 = getBits(inst, 31, 20);
+        int32_t imm = signExtend(imm12, 12);
 
-        if (rd != 0) {
-            x[rd] = result;
+        std::cout << "[CPU] (I-type) imm=" << imm << std::endl;
+
+        if (funct3 == 0x0){
+            uint32_t old = x[rs1];
+            uint32_t result = old + imm;
+
+            if(rd != 0){
+              x[rd] = result;
+            }
+           std::cout << "[CPU] Executando ADDI: x" << rd
+                     << " = x" << rs1 << "(" << old << ") + "
+                     << imm << " -> " << result << std::endl;
+        }else{
+            std::cout << "[CPU] I-type nao implementado.\n";
         }
-
-        std::cout << "[CPU] Executando ADDI: x" << rd 
-                  << " = x" << rs1 << "(" << old << ") + "
-                  << imm << " -> " << result << std::endl;
+    } else if (opcode == 0x33){
+        if (funct3 == 0x0 && funct7 == 0x00){
+            uint32_t a = x[rs1];
+            uint32_t b = x[rs2];
+            uint32_t result = a + b;
+            if(rd != 0){
+              x[rd] = result;
+            }
+            std::cout << "[CPU] Executando ADD: x" << rd
+                      << " = x" << rs1 << "(" << a << ") + "
+                      << "x" << rs2 << "(" << b << ") -> " 
+                      << result << std::endl;
+        } else if(funct3 == 0x0 && funct7 == 0x20){
+            uint32_t a = x[rs1];
+            uint32_t b = x[rs2];
+            uint32_t result = a - b;
+            if(rd != 0){
+              x[rd] = result;
+            }
+            std::cout << "[CPU] Executando SUB: x" << rd
+                      << " = x" << rs1 << "(" << a << ") - "
+                      << "x" << rs2 << "(" << b << ") -> " 
+                      << result << std::endl;
+        }else {
+            std::cout << "[CPU] R-type nao implementado.\n";
+        }
     } else {
-        std::cout << "[CPU] Instrucao desconhecida. NOP.\n";
+        std::cout << "[CPU] Opcode nao implementado.\n";
     }
-        
     pc += 4;
-
     x[0] = 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,38 @@ uint32_t encodeADDI(uint8_t rd, uint8_t rs1, int32_t imm){
         | opcode;
 }
 
-int main() {
-    std::cout << "Iniciando teste (cpu ADDI)" << std::endl;
+uint32_t encodeR(uint8_t rd, uint8_t rs1, uint8_t rs2, uint8_t funct3, uint8_t funct7){
+    uint32_t opcode = 0x33;
+    return (static_cast<uint32_t>(funct7) << 25)
+        | (static_cast<uint32_t>(rs2) << 20)
+        | (static_cast<uint32_t>(rs1) << 15)
+        | (static_cast<uint32_t>(funct3) << 12)
+        | (static_cast<uint32_t>(rd) << 7)
+        | opcode;
+}
 
-    Memory mem(64);
+int main() {
+    std::cout << "Iniciando teste (CPU ADDI/ADD/SUB)\n" << std::endl;
+
+    Memory mem(128);
     Bus bus(mem);   
     CPU cpu(bus);
 
     uint32_t inst0 = encodeADDI(1, 0, 5);
-    uint32_t inst1 = encodeADDI(2, 1, 3);
-    uint32_t inst2 = encodeADDI(3, 2, -1);
+    uint32_t inst1 = encodeADDI(2, 0, 10);
+    uint32_t inst2 = encodeR(3, 1, 2, 0x0, 0x00);
+    uint32_t inst3 = encodeR(4, 3, 1, 0x0, 0x20);
 
-    mem.write32(0, inst0);
-    mem.write32(4, inst1);
-    mem.write32(8, inst2);
+    mem.write32(0x00, inst0);
+    mem.write32(0x04, inst1);
+    mem.write32(0x08, inst2);
+    mem.write32(0x0C, inst3);
 
     std::cout << "\nDump inicial da CPU:\n";
     cpu.dump();
 
-    std::cout << "\nExecutando 3 instrucoes:\n";
+    std::cout << "\nExecutando 4 instrucoes:\n";
+    cpu.step();
     cpu.step();
     cpu.step();
     cpu.step();
